8161e621ba21f0db85741b3d6ff293955d5a9ccc
AnnaH <anglik_77@mail.ru>
2022-08-13 09:56:07 +0300

    код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿string[] array1 = new string[5] {"hello", "2", "world", ":-)", "185"};
 string[] array2 = new string[5] {"1234", "1565", "-2", "com", "git"};
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array2);
